4d5cc958470cedba41b3313a1df95d277a4f2874
Mahnoor Naeem <64680000+mahnoorn8@users.noreply.github.com>
2024-08-28 18:39:34 -0400

    Update NDWICalc.js

--- geeCode/NDWICalc.js
+++ geeCode/NDWICalc.js
@@ -1,1 +1,36 @@
 
+Map.centerObject(geometry);
+Map.addLayer(geometry);
+
+var startDate = '2017-10-14';
+var endDate = '2017-10-19';
+
+var collection = ee.ImageCollection('COPERNICUS/S2')
+                  .filterBounds(geometry)
+                  .filterDate(startDate, endDate);
+                  
+print("Number of images = ", collection.size());
+
+var sentinelImage = 
+    collection.sort('CLOUDY_PIXEL_PERCENTAGE')
+    .first()
+    .clip(geometry);
+    
+print("Sentinel image taken at = ", sentinelImage.date());
+
+
+Map.addLayer(
+    sentinelImage,
+    {min: 0, max: 2000, bands: ['B4', 'B3', 'B2']},
+    'RGB');
+
+var ndwi =
+    sentinelImage.normalizedDifference(['B3', 'B8']).rename('NDWI');
+    
+Map.addLayer(
+    ndwi,
+    {palette: ['red', 'yellow', 'green', 'cyan', 'blue']},
+    'NDWI');
+  
+var sentinelPixels = ndwi.sampleRegions(collection);
+Export.table.toDrive(sentinelPixels, 'ndwiPixels')
